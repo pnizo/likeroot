b4c4b2e1ee69d1e3967fa7206f59cfa5a5740a0c
pnizo <waka@squall.dti.ne.jp>
2024-04-14 04:54:37 +0000

    rest apiで書き直す

--- api/hello.ts
+++ api/hello.ts
@@ -1,33 +1,39 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { NeynarAPIClient, CastParamType } from "@neynar/nodejs-sdk";
+import fetch from "node-fetch"
 
 // make sure to set your NEYNAR_API_KEY .env
 //const key = "2B1AA1A8-8757-4CEE-82D2-3C37BDC70D62"
-const client = new NeynarAPIClient(String(process.env.NEYNAR_API_KEY));
+//const client = new NeynarAPIClient(String(process.env.NEYNAR_API_KEY));
+const api_key = String(process.env.NEYNAR_API_KEY);
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
 
   const url = "https://warpcast.com/pnizo.eth/0x3321413d"
 
-  const cast = await client.lookUpCastByHashOrWarpcastUrl(url, CastParamType.Url);
+  const options = {
+    method: 'GET',
+    headers: {accept: 'application/json', api_key: api_key}
+  };
 
-  console.log(cast);
-  
-      const reactions = cast['cast']['reactions'];
-      const likes = reactions['likes'];
+  const api_url = 'https://api.neynar.com/v2/farcaster/cast?identifier=' + encodeURI(url) + '&type=url';
 
-      console.log(likes);
-      
-      var msg = '';
+  console.log(api_url);
+    
+  const response = await fetch(api_url, options);
+  console.log(response);
 
-      likes.forEach((like) => { 
-        msg += like['fname'] + ' ';
-        console.log(msg); // Access the 'fname' property correctly
-      })
-      
-     const data = {
-        message: msg.substring(0, 29)
-     }
+  const data = await response.json();
+  console.log(data);
+
+    // .then(response => {
+    //   console.log(response.json());
+    //   if (!response.ok) {
+    //     throw new Error('Network response was not ok');
+    //   }
+    //   data = String(response.json());
+    // })
+    // .then(response => console.log(response))
+    // .catch(err => console.error(err));
  
   return res.json(data);
 }
