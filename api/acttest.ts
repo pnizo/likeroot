5df07e70d9b52ee519c5afd484151abb337725c6
pnizo <waka@squall.dti.ne.jp>
2024-04-14 11:48:26 +0000

    urlencode

--- api/acttest.ts
+++ api/acttest.ts
@@ -4,9 +4,11 @@ import { NeynarAPIClient, CastParamType } from "@neynar/nodejs-sdk";
 // make sure to set your NEYNAR_API_KEY .env
 const client = new NeynarAPIClient(String(process.env.NEYNAR_API_KEY));
 
-const ADD_URL = "https://warpcast.com/~/add-cast-action?url=https://likeroot-git-main-pnizos-projects.vercel.app/api/acttest";
+const ADD_URL = "https://warpcast.com/~/add-cast-action?url=https%3A%2F%2Flikeroot-git-main-pnizos-projects.vercel.app%2Fapi%2Facttest";
+//const ADD_URL = "https://warpcast.com/~/add-cast-action?postUrl="https%3A%2F%2Fcastactions.xyz%2Fremind&name=Remind%20me%20in%2010%20days+"&actionType=post&icon=heart"
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+    console.log(ADD_URL);
 
     if (req.method === 'GET') {
         const data = {
