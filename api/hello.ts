c84cc8d187e7ea2a54d5853af81e12b4c30071bd
pnizo <waka@squall.dti.ne.jp>
2024-04-14 00:17:55 +0000

    hello.tsとか

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,29 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { NeynarAPIClient, CastParamType } from "@neynar/nodejs-sdk";
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello ${name}!`,
+// make sure to set your NEYNAR_API_KEY .env
+const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY);
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+//  console.log(req);
+
+  const url = "https://warpcast.com/pnizo.eth/0x3321413d"
+
+  const cast = await client.lookUpCastByHashOrWarpcastUrl(url, CastParamType.Url);
+  
+  const reactions = cast['cast']['reactions'];
+  const likes = reactions['likes'];
+  
+  var msg = '';
+
+  likes.forEach((like) => { 
+    msg += like['fname'] + ' ';
+    console.log(like['fname']);
   })
+  
+ const data = {
+    message: msg.substring(0, 29)
+ }
+ 
+  return res.json(data);
 }
