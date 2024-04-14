0e4e513e9ed82a57b33bcac164ff1cababd17804
pnizo <waka@squall.dti.ne.jp>
2024-04-14 05:12:48 +0000

    apikey

--- api/hello.ts
+++ api/hello.ts
@@ -1,9 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 import fetch from "node-fetch"
 
-// make sure to set your NEYNAR_API_KEY .env
-//const key = "2B1AA1A8-8757-4CEE-82D2-3C37BDC70D62"
-//const client = new NeynarAPIClient(String(process.env.NEYNAR_API_KEY));
 const api_key = String(process.env.NEYNAR_API_KEY);
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
@@ -24,16 +21,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
   const data = await response.json();
   console.log(data);
-
-    // .then(response => {
-    //   console.log(response.json());
-    //   if (!response.ok) {
-    //     throw new Error('Network response was not ok');
-    //   }
-    //   data = String(response.json());
-    // })
-    // .then(response => console.log(response))
-    // .catch(err => console.error(err));
  
   return res.json(data);
 }
