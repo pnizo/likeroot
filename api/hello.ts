37dc312ccbcc8f7afb0c0827486a9b7238caeac3
pnizo <waka@squall.dti.ne.jp>
2024-04-14 02:56:20 +0000

    tsに戻す

--- api/hello.ts
+++ api/hello.ts
@@ -2,27 +2,32 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { NeynarAPIClient, CastParamType } from "@neynar/nodejs-sdk";
 
 // make sure to set your NEYNAR_API_KEY .env
-const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY);
+//const key = "2B1AA1A8-8757-4CEE-82D2-3C37BDC70D62"
+const client = new NeynarAPIClient(String(process.env.NEYNAR_API_KEY));
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
 
   const url = "https://warpcast.com/pnizo.eth/0x3321413d"
 
   const cast = await client.lookUpCastByHashOrWarpcastUrl(url, CastParamType.Url);
-  
-  const reactions = cast['cast']['reactions'];
-  const likes = reactions['likes'];
-  
-  var msg = '';
 
-  likes.forEach((like) => { 
-    msg += like['fname'] + ' ';
-    console.log(like['fname']);
-  })
+  console.log(cast);
   
- const data = {
-    message: msg.substring(0, 29)
- }
+      const reactions = cast['cast']['reactions'];
+      const likes = reactions['likes'];
+
+      console.log(likes);
+      
+      var msg = '';
+
+      likes.forEach((like) => { 
+        msg += like['fname'] + ' ';
+        console.log(msg); // Access the 'fname' property correctly
+      })
+      
+     const data = {
+        message: msg.substring(0, 29)
+     }
  
   return res.json(data);
 }
