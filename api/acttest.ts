9954584f3a3f8a4e5cb260b1973aa9044603d039
pnizo <waka@squall.dti.ne.jp>
2024-04-14 09:58:54 +0000

    acttest追加

---
+++ api/acttest.ts
@@ -0,0 +1,49 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { NeynarAPIClient, CastParamType } from "@neynar/nodejs-sdk";
+
+// make sure to set your NEYNAR_API_KEY .env
+const client = new NeynarAPIClient(String(process.env.NEYNAR_API_KEY));
+
+const ADD_URL = "https://warpcast.com/~/add-cast-action?url=https://likeroot-git-main-pnizos-projects.vercel.app/api/acttest";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+
+    if (req.method === 'GET') {
+        const data = {
+            name: "hello",
+            icon: "heart",
+            description: "test action",
+            aboutUrl: "https://x.com/pnizo",
+            action: {
+              type: "post",
+              url: ADD_URL,
+            },
+          };
+          return res.json(data);
+    }
+    else {
+        const url = "https://warpcast.com/pnizo.eth/0x3321413d"
+
+        const cast = await client.lookUpCastByHashOrWarpcastUrl(url, CastParamType.Url);
+
+        console.log(cast);
+    
+        const reactions = cast['cast']['reactions'];
+        const likes = reactions['likes'];
+
+        console.log(likes);
+        
+        var msg = '';
+
+        likes.forEach((like) => { 
+            msg += like['fname'] + ' ';
+            console.log(msg); // Access the 'fname' property correctly
+        })
+        
+        const data = {
+            message: msg.substring(0, 29)
+        }
+    
+        return res.json(data);
+    }
+}
