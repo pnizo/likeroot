1a79b8661eeecc4398d2e3dd8ee8941ea70d1c0c
pnizo <waka@squall.dti.ne.jp>
2024-06-06 04:29:17 +0000

    クエリ結果をGETパラメータに入れてFrameに投げる

--- api/degen.ts
+++ api/degen.ts
@@ -1,6 +1,8 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 const ADD_URL = "https://warpcast.com/~/add-cast-action?url=https%3A%2F%2Flikeroot-git-main-pnizos-projects.vercel.app%2Fapi%2Fdegen";
+const ENDPOINT = "https://www.degentip.me/api/get_allowance?fid=";
+const FRAME_URL = "https://degen-smoky.vercel.app/frame";
 
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
@@ -13,16 +15,64 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
             aboutUrl: "https://warpcast.com/pnizo.eth",
             action: {
               type: "post",
-              url: ADD_URL,
+              postUrl: ADD_URL,
             },
           };
           return res.json(data);
     }
     else {
-        
+        const body = await req['body']['untrustedData'];
+        const castId = body['castId']
+        const caster_fid = castId['fid'];
+        const my_fid = body['fid'];
+
+        const axios = require('axios');
+
+        const caster_query_url = ENDPOINT + caster_fid;
+
+        let caster_degen = '-1';
+        let caster_allowance = '-1';
+        let caster_rank = '-1';
+
+
+        await axios
+            .get(caster_query_url)
+            .then((response) => {
+                console.log(response.data);
+                const dat = response.data['allowance'];
+                caster_degen = String(dat['remaining_allowance']);
+                caster_allowance = String(dat['tip_allowance']);
+                caster_rank = String(dat['user_rank']);
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.json({message: 'error: something went wrong'});
+            });
+
+        const my_query_url = ENDPOINT + my_fid;
+        let my_degen = '-1';
+        let my_allowance = '-1';
+        let my_rank = '-1';
+
+        await axios
+        .get(my_query_url)
+        .then((response) => {
+            console.log(response.data);
+            const dat = response.data['allowance'];
+            my_degen = String(dat['remaining_allowance']);
+            my_allowance = String(dat['tip_allowance']);
+            my_rank = String(dat['user_rank']);
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.json({message: 'error: something went wrong'});
+        });    
+
+        const frame_url = FRAME_URL + '?caster_degen=' + caster_degen + '&caster_allowance=' + caster_allowance + '&caster_rank=' + caster_rank + '&my_degen=' + my_degen + '&my_allowance=' + my_allowance + '&my_rank=' + my_rank;
+
         const data = {
             "type": "frame",
-            "frameUrl": "https://degen-smoky.vercel.app/frame"
+            "frameUrl": frame_url
         }
     
         return res.json(data);
